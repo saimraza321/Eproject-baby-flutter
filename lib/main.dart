c88c9c8595995cba1ea4b7698fe0abc13c3b6a5e
saimraza321 <saimraza0300@gmail.com>
2025-07-02 10:49:45 +0500

    Device preview added and spalsh screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,46 @@
-import 'package:eprojectbaby/firebase_options.dart';
-import 'package:eprojectbaby/frontend/signup.dart';
-import 'package:eprojectbaby/frontend/splashscreen.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:eprojectbaby/firebase_options.dart';
+import 'package:eprojectbaby/frontend/signup.dart';
+import 'package:eprojectbaby/frontend/splashscreen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // <--- ADD THIS
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
-    MaterialApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData(
-    textTheme: GoogleFonts.poppinsTextTheme(),
-  ),
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        brightness: Brightness.dark,
+      ),
       home: Splashscreen(),
       routes: {
-        "/signup":(context)=> Signup(),
-        // "/add":(context)=> (isLoggedIn && isAdmin) ? Addproduct(): Login(),
-        // "/login":(context)=>Login(),
+        "/signup": (context) => Signup(),
+        // Add other routes as needed
       },
-      )
     );
+  }
 }
